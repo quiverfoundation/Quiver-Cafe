1c1561614562bf6a6a7895399e4fed8da96f2bae
Luke McGowan <luke@quiverfoundation.org>
2026-09-01 07:15:20 +0200

    Rework pricing with real cart/package rates, add café visit section with story video, Kamerik105 wordmark

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+var video = document.getElementById('storyVideo');
+if (video) {
+  var id = video.getAttribute('data-youtube');
+  if (id) {
+    video.addEventListener('click', function () {
+      var f = document.createElement('iframe');
+      f.src = 'https://www.youtube-nocookie.com/embed/' + encodeURIComponent(id) + '?autoplay=1&rel=0';
+      f.setAttribute('allow', 'autoplay; encrypted-media; picture-in-picture');
+      f.setAttribute('allowfullscreen', '');
+      f.className = 'story-iframe';
+      video.innerHTML = '';
+      video.appendChild(f);
+    }, { once: true });
+  }
+}
